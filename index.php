5f6c10faae07c3f416f1dbe871c46db8eee8710d
archistico <info@archistico.com>
2016-06-04 22:31:06 +0200

    Sistemazione footer che carica da file

--- index.php
+++ index.php
@@ -294,12 +294,7 @@
                 <!-- /.content -->
             </div>
             <!-- /.content-wrapper -->
-            <footer class="main-footer">
-                <div class="pull-right hidden-xs">
-                    <b>Version</b> 0.0.1
-                </div>
-                <strong>Copyright &copy; 2016 <a href="http://www.archistico.com">Archistico</a></strong> All rights reserved. - Theme by Almsaeed Studio
-            </footer>
+            <?php include 'footer.php'; ?>
         </div>
         <!-- ./wrapper -->
         

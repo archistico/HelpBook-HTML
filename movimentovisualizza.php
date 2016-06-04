3e785f264a787e77af23c25b7b38301be8dc5b6c
archistico <info@archistico.com>
2016-06-04 21:56:23 +0200

    Sistemazione INSERT e accorpamento link e script

--- movimentovisualizza.php
+++ movimentovisualizza.php
@@ -5,37 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title> HelpBook | MOVIMENTO - NUOVO</title>
         <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.6 -->
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-        <!-- daterange picker -->
-        <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker-bs3.css">
-        <!-- bootstrap datepicker -->
-        <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
-        <!-- iCheck for checkboxes and radio inputs -->
-        <link rel="stylesheet" href="plugins/iCheck/all.css">
-        <!-- Bootstrap Color Picker -->
-        <link rel="stylesheet" href="plugins/colorpicker/bootstrap-colorpicker.min.css">
-        <!-- Bootstrap time Picker -->
-        <link rel="stylesheet" href="plugins/timepicker/bootstrap-timepicker.min.css">
-        <!-- Select2 -->
-        <link rel="stylesheet" href="plugins/select2/select2.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-        <!-- AdminLTE Skins. Choose a skin from the css/skins
-             folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        <?php include 'link.php'; ?>
     </head>
     <body class="hold-transition skin-blue sidebar-mini">
         <div class="wrapper">
@@ -179,7 +149,7 @@
 
                             date_default_timezone_set('Europe/Rome');
 
-                            $db->exec("INSERT INTO helpbookdb.movimentidettaglio (idmovimentodettaglio, fkmovimento, fklibro, quantita, sconto, cancellato) VALUES (NULL, '".$idmovimento."', '".$idlibro."', '".$quantita."', '".$sconto."', '0');");
+                            $db->exec("INSERT INTO movimentidettaglio (idmovimentodettaglio, fkmovimento, fklibro, quantita, sconto, cancellato) VALUES (NULL, '".$idmovimento."', '".$idlibro."', '".$quantita."', '".$sconto."', '0');");
 
                             // chiude il database
                             $db = NULL;
@@ -214,7 +184,7 @@
 
                             date_default_timezone_set('Europe/Rome');
 
-                            $db->exec("DELETE FROM helpbookdb.movimentidettaglio WHERE movimentidettaglio.idmovimentodettaglio = ".$idmovimentodettaglio.";");
+                            $db->exec("DELETE FROM movimentidettaglio WHERE movimentidettaglio.idmovimentodettaglio = ".$idmovimentodettaglio.";");
 
                             // chiude il database
                             $db = NULL;
@@ -505,42 +475,7 @@
         </div>
         <!-- ./wrapper -->
 
-        <!-- jQuery 2.2.0 -->
-        <script src="plugins/jQuery/jQuery-2.2.0.min.js"></script>
-        <!-- Bootstrap 3.3.6 -->
-        <script src="bootstrap/js/bootstrap.min.js"></script>
-        <!-- Select2 -->
-        <script src="plugins/select2/select2.full.min.js"></script>
-        <!-- InputMask -->
-        <script src="plugins/input-mask/jquery.inputmask.js"></script>
-        <script src="plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
-        <script src="plugins/input-mask/jquery.inputmask.extensions.js"></script>
-        <!-- date-range-picker -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-        <script src="plugins/daterangepicker/daterangepicker.js"></script>
-        <!-- bootstrap datepicker -->
-        <script src="plugins/datepicker/bootstrap-datepicker.js"></script>
-        <!-- bootstrap color picker -->
-        <script src="plugins/colorpicker/bootstrap-colorpicker.min.js"></script>
-        <!-- bootstrap time picker -->
-        <script src="plugins/timepicker/bootstrap-timepicker.min.js"></script>
-        <!-- SlimScroll 1.3.0 -->
-        <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-        <!-- iCheck 1.0.1 -->
-        <script src="plugins/iCheck/icheck.min.js"></script>
-        <!-- FastClick -->
-        <script src="plugins/fastclick/fastclick.js"></script>
-        <!-- AdminLTE App -->
-        <script src="dist/js/app.min.js"></script>
-        <!-- AdminLTE for demo purposes -->
-        <script src="dist/js/demo.js"></script>
-        <!-- Page script -->
-        <script>
-            $(function () {
-                //Initialize Select2 Elements
-                $(".select2").select2(); 
-            });
-        </script>
+        <?php include 'script.php'; ?>
     </body>
 </html>
 
---
+++ script.php
@@ -0,0 +1,40 @@
+<!-- jQuery 2.2.0 -->
+<script src="plugins/jQuery/jQuery-2.2.0.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.6 -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- Morris.js charts -->
+<!-- EMILIE CANCELLATO TEMP <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script> -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/morris/morris.min.js"></script> -->
+<!-- Sparkline -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/sparkline/jquery.sparkline.min.js"></script> -->
+<!-- jvectormap -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script> -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script> -->
+<!-- jQuery Knob Chart -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/knob/jquery.knob.js"></script> -->
+<!-- daterangepicker -->
+<!-- EMILIE CANCELLATO TEMP <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script> -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/daterangepicker/daterangepicker.js"></script> -->
+<!-- datepicker -->
+<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script> -->
+<!-- Slimscroll -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script> -->
+<!-- FastClick -->
+<!-- EMILIE CANCELLATO TEMP <script src="plugins/fastclick/fastclick.js"></script> -->
+<!-- AdminLTE App -->
+<script src="dist/js/app.min.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<!-- EMILIE CANCELLATO TEMP <script src="dist/js/pages/dashboard.js"></script> -->
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+
+<!-- ChartJS 1.0.1 -->
+<script src="plugins/chartjs/Chart.min.js"></script>
